5d35fc2fc068b00cff6c2e857fb4a1956336cdb3
pablomartan <pablomartan@kolabnow.com>
2023-05-16 19:25:00 +0200

    feat: add webpack dev config file

---
+++ webpack.dev.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PORT = 3000;
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [ 'babel-loader' ]
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: PORT,
+    historyApiFallback: false,
+    open: true
+  }
+}
