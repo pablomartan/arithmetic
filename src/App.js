f08b539cfe25087c2a8f23058b3abb68bcb949c3
pablomartan <pablomartan@kolabnow.com>
2023-05-16 19:26:35 +0200

    feat: add React components

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import { Game } from './components/Game';
+
+const randomSum = () => {
+  return ''.concat(Math.round(Math.random() * 20), '+', Math.round(Math.random() * 20));
+};
+
+export const App = props => {
+  const [gameOn, setGameOn] = useState(false);
+  const [question, setQuestion] = useState(randomSum());
+  const [userInput, setUserInput] = useState('');
+
+  const rightAnswer = (question, userInput) => {
+    const newQuestion = question;
+    if (eval(question) === parseInt(userInput)) {
+      setTimeout(() => {
+        setQuestion(randomSum());
+        setUserInput('');
+        document.getElementById('answer').value = '';
+      }, 500);
+    }
+
+    return newQuestion;
+  };
+  
+  return(
+    <div>
+      <h1>This is a simple math game</h1>
+      <h2>Game is {gameOn ? 'on' : 'off' }</h2>
+      <button onClick={() => setGameOn(!gameOn)}>{gameOn ? 'Stop' : 'Start'}</button>
+    {gameOn ? <Game question={rightAnswer(question, userInput)} setUserInput={setUserInput} /> : <div />}
+    </div>
+  );
+};
---
+++ src/components/Game.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+export const Game = props => {
+  console.log(props);
+
+  const updateInput = (e, setUserInput) => {
+    const input = e.target.value;
+    setUserInput(input);
+  };
+
+
+  return(
+    <div className="game">
+      <p id="question">{props.question}</p>
+      <input id="answer" onChange={e => props.setUserInput(e.target.value)} />
+    </div>
+  );
+};
