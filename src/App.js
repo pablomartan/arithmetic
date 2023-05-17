81e2a37cbc1ee241e96d12731d0144bebf787ddd
pablomartan <pablomartan@kolabnow.com>
2023-05-17 20:10:05 +0200

    fix: return null instead of div when game stopped in App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const randomSum = () => {
   return ''.concat(Math.round(Math.random() * 20), '+', Math.round(Math.random() * 20));
 };
 
+
 export const App = props => {
   const [gameOn, setGameOn] = useState(false);
   const [question, setQuestion] = useState(randomSum());
@@ -29,7 +30,7 @@ export const App = props => {
       <h1>This is a simple math game</h1>
       <h2>Game is {gameOn ? 'on' : 'off' }</h2>
       <button onClick={() => setGameOn(!gameOn)}>{gameOn ? 'Stop' : 'Start'}</button>
-    {gameOn ? <Game question={rightAnswer(question, userInput)} setUserInput={setUserInput} /> : <div />}
+      {gameOn ? <Game question={rightAnswer(question, userInput)} setUserInput={setUserInput} /> : null}
     </div>
   );
 };
