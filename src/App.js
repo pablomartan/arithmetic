f42ddbcb90956d0aeab0fc6954e6536bfe455bce
pablomartan <pablomartan@kolabnow.com>
2023-05-17 20:19:29 +0200

    feat: move game logic to Game component

--- src/App.js
+++ src/App.js
@@ -2,35 +2,15 @@ import React, { useState } from 'react';
 
 import { Game } from './components/Game';
 
-const randomOp = operator => {
-  return ''.concat(Math.round(Math.random() * 20), operator, Math.round(Math.random() * 20));
-};
-
-
 export const App = props => {
   const [gameOn, setGameOn] = useState(false);
-  const [question, setQuestion] = useState(randomOp('+'));
-  const [userInput, setUserInput] = useState('');
-
-  const rightAnswer = (question, userInput) => {
-    const newQuestion = question;
-    if (eval(question) === parseInt(userInput)) {
-      setTimeout(() => {
-        setQuestion(randomOp('+'));
-        setUserInput('');
-        document.getElementById('answer').value = '';
-      }, 500);
-    }
-
-    return newQuestion;
-  };
   
   return(
     <div>
       <h1>This is a simple math game</h1>
       <h2>Game is {gameOn ? 'on' : 'off' }</h2>
       <button onClick={() => setGameOn(!gameOn)}>{gameOn ? 'Stop' : 'Start'}</button>
-      {gameOn ? <Game question={rightAnswer(question, userInput)} setUserInput={setUserInput} /> : null}
+      {gameOn ? <Game /> : null}
     </div>
   );
 };
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,25 @@
 import React, { useState } from 'react';
 
+const randomOp = operator => {
+  return ''.concat(Math.round(Math.random() * 20), operator, Math.round(Math.random() * 20));
+};
+
 export const Game = props => {
+  const [question, setQuestion] = useState(randomOp('+'));
+  const [userInput, setUserInput] = useState('');
+
+  const rightAnswer = (question, userInput) => {
+    const newQuestion = question;
+    if (eval(question) === parseInt(userInput)) {
+      setTimeout(() => {
+        setQuestion(randomOp('+'));
+        setUserInput('');
+        document.getElementById('answer').value = '';
+      }, 500);
+    }
+
+    return newQuestion;
+  };
 
   const updateInput = (e, setUserInput) => {
     const input = e.target.value;
@@ -10,8 +29,8 @@ export const Game = props => {
 
   return(
     <div className="game">
-      <p id="question">{props.question}</p>
-      <input id="answer" onChange={e => props.setUserInput(e.target.value)} />
+      <p id="question">{rightAnswer(question, userInput)}</p>
+      <input id="answer" onChange={e => setUserInput(e.target.value)} />
     </div>
   );
 };
