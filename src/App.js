409f83a5cbea39af7e0cbc026405ba117dfc3047
pablomartan <pablomartan@kolabnow.com>
2023-05-22 19:32:12 +0200

    feat: remove all logic from App component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import React, { useState, useEffect } from 'react';
 import { Game } from './components/Game';
 
 export const App = () => {
-  const [gameOn, setGameOn] = useState(false);
   
   return(
     <div>
       <h1>This is a simple math game</h1>
-      <h2>Game is {gameOn ? 'on' : 'off' }</h2>
-      <button onClick={() => setGameOn(!gameOn)}>{gameOn ? 'Stop' : 'Start'}</button>
-      {gameOn ? <Game /> : null}
+      <Game />
     </div>
   );
 };
