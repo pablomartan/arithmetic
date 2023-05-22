c39cc29b04dede966f59dfba4b8bcda50d5d36fd
pablomartan <pablomartan@kolabnow.com>
2023-05-22 20:14:09 +0200

    feat: add timer features to Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,16 +7,39 @@ export const Game = () => {
   const [timer, setTimer] = useState(null);
   const [timeLeft, setTimeLeft] = useState(300);
 
+  const startStopButton = () => {
+    if (!gameOn) {
+      startGame();
+    } else {
+      stopGame();
+    }
+  };
+
+  const startGame = () => {
+    setGameOn(true);
+    setTimer(setInterval(() => {
+      setTimeLeft(timeLeft => timeLeft - 1);
+    }, 1000));
+  };
+
+  const stopGame = () => {
+    setGameOn(false);
+    clearInterval(timer);
+    setTimer(null);
+    setTimeLeft(300);
+  };
+
   useEffect(() => {
+    console.log(timer, timeLeft);
     if (timeLeft <= 0) {
-      setGameOn(false);
-      setTimer(null);
+      stopGame();
     }
   });
 
   return(
     <div className="game">
-      <button id="start-button" onClick={() => setGameOn(!gameOn)}>
+      <h2>{timeLeft}</h2>
+      <button id="start-button" onClick={() => startStopButton()}>
         {gameOn ? 'Stop' : 'Start'}
       </button>
       {gameOn ? <Question /> : null}
