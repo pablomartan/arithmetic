e2fbb2d2d7461868521096c62fa1567ba90461c6
pablomartan <pablomartan@kolabnow.com>
2023-05-22 19:36:47 +0200

    feat: add game start and stop button to Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,6 +5,7 @@ const randomOp = operator => {
 };
 
 export const Game = () => {
+  const [gameOn, setGameOn] = useState(false);
   const [question, setQuestion] = useState(randomOp('+'));
   const [userInput, setUserInput] = useState('');
 
@@ -29,6 +30,7 @@ export const Game = () => {
 
   return(
     <div className="game">
+      <button id="start-button" onClick={() => setGameOn(!gameOn)}>{gameOn ? 'Stop' : 'Start'}</button>
       <p id="question">{rightAnswer(question, userInput)}</p>
       <input id="answer" onChange={e => setUserInput(e.target.value)} />
     </div>
