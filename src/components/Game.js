3b48c609a273d11cc3e1f04deb7c60caf448d3e5
pablomartan <pablomartan@kolabnow.com>
2023-05-22 21:52:48 +0200

    feat: move timer to separate component from Game

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,46 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 import { Question } from './Question';
 import { Settings } from './Settings';
+import { Timer } from './Timer';
 
 export const Game = () => {
   const [gameOn, setGameOn] = useState(false);
-  const [timer, setTimer] = useState(null);
-  const [timeLeft, setTimeLeft] = useState(300);
   const [operators, setOperators] = useState(['+']);
-
-  const startStopButton = () => {
-    if (!gameOn) {
-      startGame();
-    } else {
-      stopGame();
-    }
-  };
-
-  const startGame = () => {
-    setGameOn(true);
-    setTimer(setInterval(() => {
-      setTimeLeft(timeLeft => timeLeft - 1);
-    }, 1000));
-  };
-
-  const stopGame = () => {
-    setGameOn(false);
-    clearInterval(timer);
-    setTimer(null);
-    setTimeLeft(300);
-  };
-
-  useEffect(() => {
-    if (timeLeft <= 0) {
-      stopGame();
-    }
-  });
 
   return(
     <div className="game">
-      <h2>{timeLeft}</h2>
-      <Settings operators={operators} setOperators={setOperators} setTimeLeft={setTimeLeft} />
-      <button id="start-button" onClick={() => startStopButton()}>
+      <Timer gameOn={gameOn} setGameOn={setGameOn} />
+      <Settings operators={operators} setOperators={setOperators} />
+      <button id="start-button" onClick={() => setGameOn(!gameOn)}>
         {gameOn ? 'Stop' : 'Start'}
       </button>
       {gameOn ? <Question ops={operators} /> : null}
---
+++ src/components/Timer.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+export const Timer = props => {
+  const [gameOn, setGameOn] = [props.gameOn, props.setGameOn];
+  const [timeLeft, setTimeLeft] = useState(300);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timeLeft === 0) {
+        setGameOn(false);
+      } else if (gameOn) {
+        setTimeLeft(timeLeft => timeLeft - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  const timeInMinutes = time => {
+    let mins = Math.round(time / 60);
+    let sec = Math.round(time % 60);
+    
+    return (mins < 10 ? '0'.concat(mins) : mins)
+      + ':' + (sec < 10 ? '0'.concat(sec) : sec);
+  };
+
+  return(
+    <div id="timer">
+      <h3>Time left: {timeInMinutes(timeLeft)}</h3>
+    </div>
+  );
+};
