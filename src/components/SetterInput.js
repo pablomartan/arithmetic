4c1fb0c31bdafa937cc1837caf2b44772b6c9636
pablomartan <pablomartan@kolabnow.com>
2023-05-30 17:39:31 +0200

    refactor: add attributes depending on type on SetterInput

--- src/components/SetterInput.js
+++ src/components/SetterInput.js
@@ -5,20 +5,25 @@ export const SetterInput = props => {
 
   const type = id == 'max' ? 'number' : 'text';
 
-  return(
+  const inputAttributes = {
+    id: `${id}-input`,
+    name: `${id}-input`,
+    type: type,
+    defaultValue: value
+  }
+
+  if (type === 'text') {
+    inputAttributes.pattern = '^[\\d]{2}:[\\d]{2}';
+  } else if (type === 'number') {
+    inputAttributes.min = '1';
+  }
+
+  return (
     <div id={`${id}-settings`}>
       <label htmlFor={`${id}-input`}>
         {label}
       </label>
-      <input
-        id={`${id}-input`}
-        name={`${id}-input`}
-        type={type}
-        minLength='1'
-        maxLength='5'
-        defaultValue={value}
-        min='1'
-      />
+      <input {...inputAttributes} />
     </div>
   );
 };
