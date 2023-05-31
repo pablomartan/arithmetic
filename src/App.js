bc6ae7cbc7b2f4da7b9b004923edca5ad04d3007
pablomartan <pablomartan@kolabnow.com>
2023-05-31 15:58:12 +0200

    feat: add click handler for start-stop button in App

--- src/App.js
+++ src/App.js
@@ -11,12 +11,19 @@ export const App = () => {
   const [max, setMax] = useState(20);
   const [score, setScore] = useState(0);
 
+  /**
+   * @description: handles the start and stop of the game
+   */
+  const handleStartButton = () => {
+    setGameOn(!gameOn);
+  };
+
   return(
     <>
       {!gameOn && <Settings time={time} max={max} />}
       {gameOn && <Timer time={time} setGameOn={setGameOn} />}
       {gameOn || score > 0 && <h3 id='score'>{score}</h3>}
-      <button id='start-stop'>{gameOn ? 'Stop' : 'Start'}</button>
+      <button id='start-stop' onClick={e => handleStartButton()}>{gameOn ? 'Stop' : 'Start'}</button>
       {gameOn && <Question operations={operations} max={max} setScore={setScore} />}
     </>
   );
