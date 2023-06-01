879f8721e3b5bad685697c2c78e5c6eca44d7276
pablomartan <pablomartan@kolabnow.com>
2023-06-01 17:41:16 +0200

    feat: add production webpack config

---
+++ webpack.prod.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [ 'babel-loader' ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ]
+}
