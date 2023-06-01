2954167cde63c2da1af848950fa82b0e90866490
pablomartan <pablomartan@kolabnow.com>
2023-06-01 09:27:28 +0200

    feat: add css for Question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import { randomOp } from '../helperFunctions';
+import './Question.css';
 
 export const Question = props => {
   const [question, setQuestion] = useState(randomOp(props.operations, props.max));
